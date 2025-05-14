ed9aa1ef36981d0294cf478be6c2c67dcac99c3e
Duane Moody <duane_moody@yahoo.com>
2025-05-14 09:33:37 -0700

    initial commit

---
+++ CivitaiX.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         Civitai X
+// @namespace    Civitai-X
+// @version      2025-05-13
+// @description  Some new features for working with Civitai
+// @author       Duane Moody
+// @match        https://civitai.com
+// @include      https://civitai.com/*
+// @connect      civitai.com
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=civitai.com
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Make prompt gen count badges into buttons to click all the that prompt's gens' checkboxes
+    const	generationsColumn	= 'div.mantine-1vbsd0c',
+ 			gensCountBadge		= 'div.mantine-Badge-root',
+			prmptSelector		= 'div.mantine-1s7mjhu',
+			cbSelector			= 'input[type="checkbox"]',
+      badgeEarlyAccess	= 'div.mantine-1503eym',
+      modelTitle			= 'div.mantine-pd27lf',
+      nolo				= null;
+
+    // div.mantine-1503eym {background-color: orange;}
+    //.mantine-pd27lf {-webkit-text-stroke: 0.2px black;}
+
+    // Delegate this to the ancestor column div
+
+    GM_addStyle("div.mantine-1503eym { background-color: orange; } .mantine-pd27lf {-webkit-text-stroke: 0.2px black;}");
+
+    if (document.querySelector(generationsColumn)) {
+        document.querySelector(generationsColumn).addEventListener('click', e => {
+            e.target.closest(gensCountBadge) && e.target.closest(prmptSelector).querySelectorAll(cbSelector).forEach(cb => {cb.click()})
+        }, false);
+    }
+
+
+    //GM_addStyle("body { color: white; background-color: black } img { border: 0 }");
+
+
+
+})();
