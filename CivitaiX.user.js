edc88c6fb7b9bb8e8d063d7167733f309c6493fb
Duane Moody <duane_moody@yahoo.com>
2025-05-14 14:50:06 -0700

    improved CSS
    Title outline improved, model "Save To" height now proportional to viewport, models grid changed from 3 to 4 horizontally

--- CivitaiX.user.js
+++ CivitaiX.user.js
@@ -1,40 +1,41 @@
 // ==UserScript==
 // @name         Civitai X
 // @namespace    Civitai-X
-// @version      1.0.6
+// @version      1.0.16
 // @description  Some new features for working with Civitai
 // @author       Duane Moody
 // @match        https://civitai.com
 // @include      https://civitai.com/*
 // @connect      civitai.com
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=civitai.com
+// @updateURL    https://github.com/duanemoody/CivitaiX/raw/refs/heads/main/CivitaiX.user.js
 // @downloadURL  https://github.com/duanemoody/CivitaiX/raw/refs/heads/main/CivitaiX.user.js
 // @grant        GM_addStyle
+
 // ==/UserScript==
 
 (function() {
     'use strict';
+    console.info('CivitaiX running');
 
-    // Make prompt gen count badges into buttons to click all the that prompt's gens' checkboxes
     const	generationsColumn	= 'div.mantine-1avyp1d',
 		gCol			= document.querySelector(generationsColumn),
+	    	body = document.querySelector('body'),
  		gensCountBadge		= 'div.mantine-Badge-root',
 		prmptSelector		= 'div.mantine-1s7mjhu',
 		cbSelector		= 'input[type="checkbox"]',
 		badgeEarlyAccess	= 'div.mantine-1503eym',
 		modelTitle		= 'div.mantine-pd27lf',
+        	modelGrid		= 'div.mantine-1ofgurw',
+	    	saveTo			= 'div.mantine-1usso6i',
 		nolo			= null;
 
     // CSS:
-    GM_addStyle(`${badgeEarlyAccess} {background-color: orange;} ${modelTitle} {-webkit-text-stroke: 0.2px black;}`);
+    GM_addStyle(`${badgeEarlyAccess} {background-color: orange;} ${saveTo} {max-height:70vh;} ${modelGrid} {grid-template-columns: repeat(4, 25%)!important;} ${modelTitle} {-webkit-text-stroke: 2px black;
+  paint-order: stroke fill;}`);
 
-    // Delegate this to the ancestor column div
-    gCol && gCol.addEventListener('click', e => {e.target.closest(gensCountBadge) && e.target.closest(prmptSelector).querySelectorAll(cbSelector).forEach(cb => {cb.click()})}, false);
-    
-
-
-    //GM_addStyle("body { color: white; background-color: black } img { border: 0 }");
-
-
+    // Make prompt gen count badges into buttons to click all the that prompt's gens' checkboxes
+    // Delegate this to body because the column may not exist at page load
+    body.addEventListener('click', e => {e.target.closest(gensCountBadge) && e.target.closest(prmptSelector).querySelectorAll(cbSelector).forEach(cb => {cb.click()})}, false);
 
 })();
