16172d70f1d515045e054f144adebd66ab6693c8
Duane Moody <duane_moody@yahoo.com>
2025-05-14 21:22:55 -0700

    Convert selectors to an array, break out event bind into sections
    Selectors changed from individual constants to an associative array named `sel`, the delegated event bound to the body element broken into event type and which selector it's targeting for easier future expansion adding other event types and targets.

--- CivitaiX.user.js
+++ CivitaiX.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Civitai X
 // @namespace    Civitai-X
-// @version      1.0.16
+// @version      1.0.18
 // @description  Some new features for working with Civitai
 // @author       Duane Moody
 // @match        https://civitai.com
@@ -11,31 +11,44 @@
 // @updateURL    https://github.com/duanemoody/CivitaiX/raw/refs/heads/main/CivitaiX.user.js
 // @downloadURL  https://github.com/duanemoody/CivitaiX/raw/refs/heads/main/CivitaiX.user.js
 // @grant        GM_addStyle
-
 // ==/UserScript==
 
 (function() {
     'use strict';
     console.info('CivitaiX running');
 
-    const	generationsColumn	= 'div.mantine-1avyp1d',
-		gCol			= document.querySelector(generationsColumn),
-	    	body = document.querySelector('body'),
- 		gensCountBadge		= 'div.mantine-Badge-root',
-		prmptSelector		= 'div.mantine-1s7mjhu',
-		cbSelector		= 'input[type="checkbox"]',
-		badgeEarlyAccess	= 'div.mantine-1503eym',
-		modelTitle		= 'div.mantine-pd27lf',
-        	modelGrid		= 'div.mantine-1ofgurw',
-	    	saveTo			= 'div.mantine-1usso6i',
-		nolo			= null;
+	const 
+		body = document.querySelector('body'),
+		// Selectors are defined here because mantine could change them later
+		sel = {
+			generationsColumn:	'div.mantine-1avyp1d',
+			gensCountBadge:		'div.mantine-Badge-root',
+			prmptSelector:		'div.mantine-1s7mjhu',
+			cbSelector:			'input[type="checkbox"]',
+			badgeEarlyAccess:	'div.mantine-1503eym',
+			modelTitle:			'div.mantine-pd27lf',
+			modelGrid:			'div.mantine-1ofgurw',
+			saveTo:				'div.mantine-1usso6i'
+		};
 
-    // CSS:
-    GM_addStyle(`${badgeEarlyAccess} {background-color: orange;} ${saveTo} {max-height:70vh;} ${modelGrid} {grid-template-columns: repeat(4, 25%)!important;} ${modelTitle} {-webkit-text-stroke: 2px black;
-  paint-order: stroke fill;}`);
+    // CSS: Don't use Greasemonkey because they removed this method
+	GM_addStyle(`
+		${sel.badgeEarlyAccess} {background-color: orange;} 
+		/* ${sel.modelGrid}	{grid-template-columns: repeat(4, 25%)!important;} */ 
+		${sel.modelTitle} {
+			-webkit-text-stroke: 2px black; 
+			paint-order: stroke fill;
+		} 
+		${sel.saveTo} {max-height:70vh;} 
+		${sel.gensCountBadge} {cursor: pointer;}
+	`);
 
-    // Make prompt gen count badges into buttons to click all the that prompt's gens' checkboxes
-    // Delegate this to body because the column may not exist at page load
-    body.addEventListener('click', e => {e.target.closest(gensCountBadge) && e.target.closest(prmptSelector).querySelectorAll(cbSelector).forEach(cb => {cb.click()})}, false);
+    // Add event listeners
+    body.addEventListener(
+		'click', e => {
+		    /* Make prompt gen count badges into buttons to click all the that prompt's gens' checkboxes. Raw clicks are sent instead of directly changing checkbox values because the download/delete buttons don't actually reference checkbox values. */
+			e.target.closest(sel.gensCountBadge) && e.target.closest(sel.prmptSelector).querySelectorAll(sel.cbSelector).forEach(cb => {cb.click()});
+		}
+	, false);
 
 })();
